f9e200e8a38a0a083952a597f52d7d34a022b508
Anna Brunkhorst <abrunkhorst510@gmail.com>
2023-11-25 09:59:52 -0500

    Revert "Show all player hands during game"
    This reverts commit 6bbaf753ef80039caa418ae2e9ee34e4a54bcebb.

--- Command-Line-Poker-master/src/poker/players/computer.py
+++ Command-Line-Poker-master/src/poker/players/computer.py
@@ -29,12 +29,12 @@ class Computer(Player):
             return self.risky_play(table_raise_amount, times_table_raised, last_table_bet)
         else:
             return self.random_play(table_raise_amount, times_table_raised, last_table_bet)
-    """
+
     def expectiminimaxGetAction(self, table_raise_amount: int, num_times_table_raised: int, table_last_best: int) -> BettingMove:
         maxActionCost = -9999
         bestAction = BettingMove.FOLDED
         for
-    """
+
     def risky_play(self, table_raise_amount: int, num_times_table_raised: int, table_last_bet: int) -> BettingMove:
         """Computer choice to check, call, raise, bet, fold, or go all-in. Player more likely to bet and raise."""
         x = random.random()
